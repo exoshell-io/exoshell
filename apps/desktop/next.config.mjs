123241f3790a1ed9f9ce199c16f5b05c57a44bbe
Charles Tison <ctison@pm.me>
2023-11-19 13:11:45 +0800

    chore(desktop): setup tracing

--- apps/desktop/next.config.mjs
+++ apps/desktop/next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
   output: 'export',
   experimental: {
     typedRoutes: true,
   },
 };
-
-export default nextConfig;
