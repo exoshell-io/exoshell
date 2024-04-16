1395d1e9c38e1229ae924dba40df0b1322dc83af
Charles Tison <ctison@pm.me>
2024-04-16 13:12:15 +0200

    feat: desktop persist localStorage

--- apps/desktop/app/_state/index.ts
+++ apps/desktop/app/_state/index.ts
@@ -1,6 +1,7 @@
 'use client';
 
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 // #region Types
 
@@ -43,119 +44,127 @@ export type Store = {
   deleteDashboard: (id: string) => void;
 };
 
-export const useStore = create<Store>()((set) => ({
-  // Layout
-  tabs: [],
-  activeTab: 0,
-  setActiveTab: (activeTab?: number | null) =>
-    set({ activeTab: activeTab ?? undefined }),
-  openTab: (href: string, focus = true) => {
-    set((state) => {
-      const index = state.tabs.findIndex((tab) => tab.href === href);
-      if (index === -1) {
-        state.tabs.push({ href });
-        if (focus) {
-          state.activeTab = state.tabs.length - 1;
-        }
-      } else if (focus) {
-        state.activeTab = index;
-      }
-      return { tabs: [...state.tabs], activeTab: state.activeTab };
-    });
-  },
-  closeTab: (index: number) => {
-    set((state) => {
-      if (index < 0 || index >= state.tabs.length) {
-        return state;
-      }
-      if (state.activeTab === index) {
-        if (state.tabs.length === 1) {
-          state.activeTab = undefined;
-        } else if (index >= state.tabs.length - 1) {
-          state.activeTab = state.tabs.length - 2;
-        }
-      }
-      state.tabs.splice(index, 1);
-      if (
-        state.activeTab !== undefined &&
-        state.activeTab >= state.tabs.length
-      ) {
-        state.activeTab--;
-      }
-      return { tabs: [...state.tabs], activeTab: state.activeTab };
-    });
-  },
+export const useStore = create<Store>()(
+  persist(
+    (set) => ({
+      // Layout
+      tabs: [],
+      activeTab: 0,
+      setActiveTab: (activeTab?: number | null) =>
+        set({ activeTab: activeTab ?? undefined }),
+      openTab: (href: string, focus = true) => {
+        set((state) => {
+          const index = state.tabs.findIndex((tab) => tab.href === href);
+          if (index === -1) {
+            state.tabs.push({ href });
+            if (focus) {
+              state.activeTab = state.tabs.length - 1;
+            }
+          } else if (focus) {
+            state.activeTab = index;
+          }
+          return { tabs: [...state.tabs], activeTab: state.activeTab };
+        });
+      },
+      closeTab: (index: number) => {
+        set((state) => {
+          if (index < 0 || index >= state.tabs.length) {
+            return state;
+          }
+          if (state.activeTab === index) {
+            if (state.tabs.length === 1) {
+              state.activeTab = undefined;
+            } else if (index >= state.tabs.length - 1) {
+              state.activeTab = state.tabs.length - 2;
+            }
+          }
+          state.tabs.splice(index, 1);
+          if (
+            state.activeTab !== undefined &&
+            state.activeTab >= state.tabs.length
+          ) {
+            state.activeTab--;
+          }
+          return { tabs: [...state.tabs], activeTab: state.activeTab };
+        });
+      },
 
-  // Workflows
-  workflows: {},
-  createWorkflow: (focus = false) => {
-    const id = `${Date.now()}-${Math.random()}`;
-    set((state) => {
-      state.workflows[id] = { name: 'Untitled', id };
-      state.tabs.push({ href: `workflow://${id}` });
-      if (focus) {
-        state.activeTab = state.tabs.length - 1;
-      }
-      return {
-        workflows: { ...state.workflows },
-        tabs: [...state.tabs],
-        activeTab: state.activeTab,
-      };
-    });
-  },
-  updateWorkflow: (id: string, workflow: Workflow) => {
-    set((state) => {
-      state.workflows[id] = workflow;
-      return { workflows: { ...state.workflows } };
-    });
-  },
-  deleteWorkflow: (id: string) => {
-    set((state) => {
-      delete state.workflows[id];
-      filterTab(`workflow://${id}`, state);
-      return {
-        workflows: { ...state.workflows },
-        tabs: [...state.tabs],
-        activeTab: state.activeTab,
-      };
-    });
-  },
+      // Workflows
+      workflows: {},
+      createWorkflow: (focus = false) => {
+        const id = `${Date.now()}-${Math.random()}`;
+        set((state) => {
+          state.workflows[id] = { name: 'Untitled', id };
+          state.tabs.push({ href: `workflow://${id}` });
+          if (focus) {
+            state.activeTab = state.tabs.length - 1;
+          }
+          return {
+            workflows: { ...state.workflows },
+            tabs: [...state.tabs],
+            activeTab: state.activeTab,
+          };
+        });
+      },
+      updateWorkflow: (id: string, workflow: Workflow) => {
+        set((state) => {
+          state.workflows[id] = workflow;
+          return { workflows: { ...state.workflows } };
+        });
+      },
+      deleteWorkflow: (id: string) => {
+        set((state) => {
+          delete state.workflows[id];
+          filterTab(`workflow://${id}`, state);
+          return {
+            workflows: { ...state.workflows },
+            tabs: [...state.tabs],
+            activeTab: state.activeTab,
+          };
+        });
+      },
 
-  // Dashboards
-  dashboards: {},
-  createDashboard: (focus = false) => {
-    const id = `${Date.now()}-${Math.random()}`;
-    set((state) => {
-      state.dashboards[id] = { name: 'Untitled', id };
-      state.tabs.push({ href: `dashboard://${id}` });
-      if (focus) {
-        state.activeTab = state.tabs.length - 1;
-      }
-      return {
-        dashboards: { ...state.dashboards },
-        tabs: [...state.tabs],
-        activeTab: state.activeTab,
-      };
-    });
-  },
-  updateDashboard: (id: string, dashboard: Dashboard) => {
-    set((state) => {
-      state.dashboards[id] = dashboard;
-      return { dashboards: { ...state.dashboards } };
-    });
-  },
-  deleteDashboard: (id: string) => {
-    set((state) => {
-      delete state.dashboards[id];
-      filterTab(`dashboard://${id}`, state);
-      return {
-        dashboards: { ...state.dashboards },
-        tabs: [...state.tabs],
-        activeTab: state.activeTab,
-      };
-    });
-  },
-}));
+      // Dashboards
+      dashboards: {},
+      createDashboard: (focus = false) => {
+        const id = `${Date.now()}-${Math.random()}`;
+        set((state) => {
+          state.dashboards[id] = { name: 'Untitled', id };
+          state.tabs.push({ href: `dashboard://${id}` });
+          if (focus) {
+            state.activeTab = state.tabs.length - 1;
+          }
+          return {
+            dashboards: { ...state.dashboards },
+            tabs: [...state.tabs],
+            activeTab: state.activeTab,
+          };
+        });
+      },
+      updateDashboard: (id: string, dashboard: Dashboard) => {
+        set((state) => {
+          state.dashboards[id] = dashboard;
+          return { dashboards: { ...state.dashboards } };
+        });
+      },
+      deleteDashboard: (id: string) => {
+        set((state) => {
+          delete state.dashboards[id];
+          filterTab(`dashboard://${id}`, state);
+          return {
+            dashboards: { ...state.dashboards },
+            tabs: [...state.tabs],
+            activeTab: state.activeTab,
+          };
+        });
+      },
+    }),
+    {
+      name: 'exoshell',
+      version: 0,
+    },
+  ),
+);
 
 function filterTab(tabId: string, state: Store) {
   state.tabs = state.tabs.filter((tab) => !tab.href.startsWith(tabId));
