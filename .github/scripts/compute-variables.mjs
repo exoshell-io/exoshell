824941dcb0ad3d34b4211ab1793e24c4a4cac280
Abraham Guimbao <abrahamguimbao@gmail.com>
2024-04-16 02:35:49 +0200

    cicd: rework config vars for all workflow types, update bun and cargo

--- .github/scripts/compute-variables.mjs
+++ .github/scripts/compute-variables.mjs
@@ -3,98 +3,61 @@
  * @param {import('@actions/core')} core
  */
 export default async function (context, core) {
-  // Gets workflow_dispatch event context
-  const workflowDispatchEvent =
-    /** @type {import('@octokit/webhooks-types').WorkflowDispatchEvent?} */ (
-      context.payload
-    );
+  const { Octokit } = require('@octokit/rest');
 
-  // Get environment from PR body
-  const _environment = context.payload?.pull_request?.body?.match(
-    /\[environment=([a-z]+)]/,
-  );
+  // Initialize Octokit with a GitHub token
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
 
-  const environment = _environment
-    ? _environment[1]
-    : /** @type {string} */ (
-        workflowDispatchEvent?.inputs?.['environment'] ?? 'dev'
-      );
+  // Config default values
+  let config = [
+    // ['environment', 'prod'],     // ['dev', 'staging', 'prod']
+    ['skip-ci', 'false'], // ['true', 'false']
+    ['always-cd', 'false'], // ['true', 'false']
+    // ['dry-run-cd', 'false'],     // ['true', 'false']
+    ['version', ''], // ['<semver>']
+    ['release-type', 'stable'], // ['stable', 'draft', 'prerelease']
+    ['target-builds', 'all'], // ['all', 'desktop', 'cli', 'mobile', 'web']
+    ['target-platforms', 'all'], // ['all', 'linux', 'macos', 'windows']
+    // Generated and injected at runtime:
+    // ['should-ci']
+    // ['should-desktop-cd']
+    // ['target-platforms-matrix']
+  ];
 
-  // Get always-ci from PR body
-  const _always_ci = context.payload?.pull_request?.body?.match(
-    /\[always-ci=([a-z]+)]/,
-  );
+  // If triggered by tag push, overrides default config with last commit body config (if exists)
+  // If triggered by pull_request, overrides default config with pull_request body config (if exists)
+  // If triggered by workflow_dispatch, overrides default config with workflow_dispatch input config (if exists)
+  if (context.eventName === 'pull_request') {
+    const _pr_body = context.payload.pull_request.body;
+    updateConfigFromText(_pr_body, config);
+  } else if (
+    context.eventName === 'push' &&
+    context.ref.startsWith('refs/tags/')
+  ) {
+    const _commit_sha = context.payload.after;
+    const _commit_data = await octokit.rest.git.getCommit({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      commit_sha: _commit_sha,
+    });
+    updateConfigFromText(_commit_data.data.message, config);
+    // Override version config value from the tag
+    const _tag_ref = context.ref;
+    let tag_version = _tag_ref.startsWith('refs/tags/v')
+      ? _tag_ref.substring('refs/tags/v'.length)
+      : _tag_ref.substring('refs/tags/desktop/v'.length);
+    // Find the version in config and update it
+    const _version_index = config.findIndex((item) => item[0] === 'version');
+    if (_version_index !== -1) {
+      config[_version_index][1] = tag_version;
+    }
+  } else if (context.eventName === 'workflow_dispatch') {
+    config = config.map(([key, _]) => [key, core.getInput(key) || _]);
+  }
 
-  const always_ci = _always_ci
-    ? _always_ci[1]
-    : /** @type {string} */ (
-        workflowDispatchEvent?.inputs?.['always-ci'] ?? 'true'
-      );
-
-  // Get always-cd from PR body
-  const _always_cd = context.payload?.pull_request?.body?.match(
-    /\[always-cd=([a-z]+)]/,
-  );
-
-  const always_cd = _always_cd
-    ? _always_cd[1]
-    : /** @type {string} */ (
-        workflowDispatchEvent?.inputs?.['always-cd'] ?? 'false'
-      );
-
-  // Get dry-run-cd from PR body
-  const _dry_run_cd = context.payload?.pull_request?.body?.match(
-    /\[dry-run-cd=([a-z]+)]/,
-  );
-
-  const dry_run_cd = _dry_run_cd
-    ? _dry_run_cd[1]
-    : /** @type {string} */ (
-        workflowDispatchEvent?.inputs?.['dry-run-cd'] ?? 'false'
-      );
-
-  // Get version from PR body
-  const _version =
-    context.payload?.pull_request?.body?.match(/\[version=([a-z]+)]/);
-
-  const version = _version
-    ? _version[1]
-    : /** @type {string} */ (workflowDispatchEvent?.inputs?.['version'] ?? '');
-
-  // Get release-type from PR body
-  const _release_type = context.payload?.pull_request?.body?.match(
-    /\[release-type=([a-z]+)]/,
-  );
-
-  const release_type = _release_type
-    ? _release_type[1]
-    : /** @type {string} */ (
-        workflowDispatchEvent?.inputs?.['release-type'] ?? 'stable'
-      );
-
-  // Get target-builds from PR body
-  const _target_builds = context.payload?.pull_request?.body?.match(
-    /\[target-builds=([a-z]+)]/,
-  );
-
-  const target_builds = _target_builds
-    ? _target_builds[1]
-    : /** @type {string} */ (
-        workflowDispatchEvent?.inputs?.['target-builds'] ?? 'all'
-      );
-
-  // Get target-platforms from PR body
-  const _target_platforms = context.payload?.pull_request?.body?.match(
-    /\[target-platforms=([a-z]+)]/,
-  );
-
-  const target_platforms = _target_platforms
-    ? _target_platforms[1]
-    : /** @type {string} */ (
-        workflowDispatchEvent?.inputs?.['target-platforms'] ?? 'all'
-      );
-
-  // Define platform settings
+  // Define 'target-platforms-matrix'
   const platforms = {
     linux: { platform: 'ubuntu-22.04', args: '' },
     macos: [
@@ -103,46 +66,64 @@ export default async function (context, core) {
     ],
     windows: { platform: 'windows-latest', args: '' },
   };
-
+  const target_platforms = config.find(
+    (item) => item[0] === 'target-platforms',
+  )[1];
   let matrix = [];
   if (target_platforms === 'all') {
     // Include all platforms if 'all' is specified
     matrix = [platforms.linux, ...platforms.macos, platforms.windows];
   } else {
     // Only include specified platforms
-    const platformsRequested = target_platforms.split(',');
-    platformsRequested.forEach((platformKey) => {
-      switch (platformKey) {
-        case 'linux':
-          matrix.push(platforms.linux);
-          break;
-        case 'macos':
-          matrix = matrix.concat(platforms.macos);
-          break;
-        case 'windows':
-          matrix.push(platforms.windows);
-          break;
+    const platforms_requested = target_platforms.split(',');
+    platforms_requested.forEach((platformKey) => {
+      if (platforms[platformKey]) {
+        const platform_to_add = platforms[platformKey];
+        Array.isArray(platform_to_add)
+          ? matrix.push(...platform_to_add)
+          : matrix.push(platform_to_add);
       }
     });
   }
-
   const target_platforms_matrix = JSON.stringify({ settings: matrix });
+  config.push(['target-platforms-matrix', target_platforms_matrix]);
+
+  // Define 'should-ci' and 'should-desktop-cd'
+  const should_ci =
+    config.find((item) => item[0] === 'skip-ci')[1] === 'true'
+      ? 'false'
+      : 'true';
+  const should_desktop_cd =
+    config.find((item) => item[0] === 'always-cd')[1] === 'true' ||
+    (context.eventName === 'push' &&
+      /^refs\/tags\/(v|desktop\/v)/.test(context.ref)) ||
+    ['all', 'desktop'].includes(
+      config.find((item) => item[0] === 'target-builds')[1],
+    )
+      ? 'true'
+      : 'false';
+  config.push(['should-ci', should_ci]);
+  config.push(['should-desktop-cd', should_desktop_cd]);
 
   core.summary.addHeading('Computed variables', 2);
 
-  computeOutputs(core, [
-    ['environment', environment],
-    ['always-ci', always_ci],
-    ['always-cd', always_cd],
-    ['dry-run-cd', dry_run_cd],
-    ['version', version],
-    ['release-type', release_type],
-    ['target-builds', target_builds],
-    ['target-platforms', target_platforms],
-    ['target-platforms-matrix', target_platforms_matrix],
-  ]);
+  computeOutputs(core, config);
 
   await core.summary.write();
+}
+
+/**
+ * @param {string} text
+ * @param {Array<Array<string>>} config
+ */
+function updateConfigFromText(text, config) {
+  config.forEach((item) => {
+    const regex = new RegExp(`\\[${item[0]}=([^\\]]+)\\]`, 'i');
+    const match = text.match(regex);
+    if (match) {
+      item[1] = match[1];
+    }
+  });
 }
 
 /**
