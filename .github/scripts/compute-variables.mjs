3c5c837b1c5908d5329451ac1cb7a5d135b52eb7
Charles Tison <ctison@pm.me>
2024-05-22 09:35:38 +0200

    cicd: simplify

--- .github/scripts/compute-variables.mjs
+++ .github/scripts/compute-variables.mjs
@@ -1,192 +1,97 @@
-import { Octokit } from '@octokit/rest';
+/** @typedef {import('@actions/github/lib/context.js').Context} GithubContext */
+/** @typedef {import('@actions/core')} GithubCore */
+/** @typedef {typeof defaultOutputs} Outputs */
+
+import { readFileSync } from 'fs';
+
+const defaultOutputs = {
+  /** @type {false | 'prerelease' | 'stable'} */
+  shouldRelease: false,
+  appVersion: '0.0.0',
+  cdTauriMatrix: '{}',
+  rustVersion: '',
+};
+
+const defaultCdTauriMatrix = {
+  include: [
+    { platform: 'ubuntu-22.04', tauriBuildTarget: 'x86_64-unknown-linux-gnu' },
+    { platform: 'macos-latest', tauriBuildTarget: 'aarch64-apple-darwin' },
+    { platform: 'macos-latest', tauriBuildTarget: 'x86_64-apple-darwin' },
+    // { platform: 'windows-latest', tauriBuildTarget: 'x86_64-pc-windows-msvc' },
+    // { platform: 'macos-latest', tauriBuildArgs: '--target universal-apple-darwin', rustTargets: 'x86_64-apple-darwin,aarch64-apple-darwin' },
+  ],
+};
 
 /**
- * @param {import('@actions/github/lib/context.js').Context} context
- * @param {import('@actions/core')} core
+ * Regex to extract the version from the git tag ref
+ */
+const tagRegex =
+  /^refs\/tags\/(?:desktop\/)?v(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)(?:-(?<channel>alpha|beta|rc)\.(?<channelPatch>\d+))?$/;
+
+/**
+ * Compute variables for the Github Actions workflow
+ * @param {GithubContext} context
+ * @param {GithubCore} core
  */
 export default async function (context, core) {
-  // Generated and injected at runtime:
-  // ['should-ci']
-  // ['should-desktop-cd']
-  // ['platforms-matrix']
-  // ['universal-version']
-  // ['is-prerelease']
+  /** @type {Outputs} */
+  const outputs = { ...defaultOutputs };
 
-  // Base config default values
-  let base_config = [];
-
-  // Admin config default values
-  let admin_config = [
-    // ['environment', 'dev'], // ['dev', 'staging', 'prod']
-    ['skip-ci', 'false'], // ['true', 'false']
-    ['always-cd', 'false'], // ['true', 'false']
-    // ['dry-run-cd', 'false'], // ['true', 'false']
-    ['version', ''], // ['<semver>']
-    // ['is-draft-release', 'false'], // ['true', 'false']
-    ['builds', 'all'], // ['all', 'desktop', 'cli', 'mobile', 'web']
-    ['platforms', 'all'], // ['all', 'linux', 'macos', 'windows']
-  ];
-
-  // Only if workflow is triggered from 'push' or 'workflow_dispatch' we allow admin config overwrite
-  let config = [...base_config];
   if (
-    context.eventName === 'push' ||
-    context.eventName === 'workflow_dispatch'
+    context.eventName === 'push' &&
+    (context.ref.startsWith('refs/tags/v') ||
+      context.ref.startsWith('refs/tags/desktop/v'))
   ) {
-    config = [...config, ...admin_config];
-    await updateConfig(context, core, config);
-  } else if (context.eventName === 'pull_request') {
-    await updateConfig(context, core, config);
-    config = [...config, ...admin_config];
+    const matches = context.ref.match(tagRegex)?.groups;
+    if (
+      matches === undefined ||
+      !('major' in matches) ||
+      !('minor' in matches) ||
+      !('patch' in matches)
+    ) {
+      throw new Error(`Invalid tag: ${context.ref} not matching ${tagRegex}`);
+    }
+    const major = parseInt(matches['major']);
+    const minor = parseInt(matches['minor']);
+    const patch = parseInt(matches['patch']);
+    const channel = matches['channel'];
+    const channelPatch =
+      'channelPatch' in matches ? parseInt(matches['channelPatch']) : undefined;
+
+    if (channel === undefined) {
+      outputs.shouldRelease = 'stable';
+      outputs.appVersion = `${major}.${minor}.${patch}`;
+    } else {
+      outputs.shouldRelease = 'prerelease';
+      const encodedPatch =
+        patch * 2048 +
+        castNonNull({ alpha: 0, beta: 1, rc: 2 }[channel]) * 32 +
+        castNonNull(channelPatch);
+      outputs.appVersion = `${major}.${minor}.${encodedPatch}`;
+    }
   }
 
-  runtimeConfig(context, config);
+  outputs.cdTauriMatrix = JSON.stringify(defaultCdTauriMatrix, undefined, 2);
+
+  // #region Extract the tools version from `.mise.toml`
+  const miseToml = readFileSync(
+    `${process.env['GITHUB_WORKSPACE']}/.mise.toml`,
+  ).toString();
+  outputs.rustVersion = castNonNull(
+    castNonNull(miseToml.match(/^rust = '(.+)'$/m))[1],
+  );
+  // #endregion
 
   core.summary.addHeading('Computed variables', 2);
-
-  computeOutputs(core, config);
-
+  computeOutputs(core, outputs);
   await core.summary.write();
 }
 
 /**
- * @param {import('@actions/github/lib/context.js').Context} context
- * @param {import('@actions/core')} core
- * @param {Array<Array<string>>} config
- */
-async function updateConfig(context, core, config) {
-  // Initialize Octokit with a GitHub token
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN,
-  });
-
-  // If triggered by tag push, overrides default config with last commit body config (if exists)
-  // If triggered by pull_request, overrides default config with pull_request body config (if exists)
-  // If triggered by workflow_dispatch, overrides default config with workflow_dispatch input config (if exists)
-  // If multiple ocurrences are found only the first one is kept
-  if (context.eventName === 'pull_request') {
-    const _pr_body = context.payload.pull_request.body || '';
-    overrideConfigFromText(_pr_body, config);
-  } else if (context.eventName === 'push') {
-    const _tag_name = context.ref.startsWith('refs/tags/v')
-      ? context.ref.substring('refs/tags/'.length)
-      : context.ref.substring('refs/tags/desktop/'.length);
-    const _tag_version = context.ref.startsWith('refs/tags/v')
-      ? context.ref.substring('refs/tags/v'.length)
-      : context.ref.substring('refs/tags/desktop/v'.length);
-    const _ref_data = await octokit.rest.git.getRef({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      ref: 'tags/' + _tag_name,
-    });
-    const { data: _tag_data } = await octokit.rest.git.getTag({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      tag_sha: _ref_data.data.object.sha,
-    });
-    overrideConfigFromText(
-      '[version=' + _tag_version + ']' + _tag_data.message,
-      config,
-    );
-  } else if (context.eventName === 'workflow_dispatch') {
-    config = config.map(([key, _]) => [key, core.getInput(key) || _]);
-  }
-}
-
-/**
- * @param {import('@actions/github/lib/context.js').Context} context
- * @param {Array<Array<string>>} config
- */
-function runtimeConfig(context, config) {
-  // Define 'platforms-matrix'
-  const platforms_list = {
-    linux: { platform: 'ubuntu-22.04', args: '' },
-    macos: [
-      { platform: 'macos-latest', args: '--target aarch64-apple-darwin' },
-      { platform: 'macos-latest', args: '--target x86_64-apple-darwin' },
-      { platform: 'macos-latest', args: '--target universal-apple-darwin' },
-    ],
-    windows: { platform: 'windows-latest', args: '' },
-  };
-  const platforms = config.find((item) => item[0] === 'platforms')[1];
-  let matrix = [];
-  if (platforms === 'all') {
-    matrix = [
-      platforms_list.linux,
-      ...platforms_list.macos,
-      platforms_list.windows,
-    ];
-  } else {
-    const platforms_requested = platforms.split(',');
-    platforms_requested.forEach((platformKey) => {
-      if (platforms_list[platformKey]) {
-        const platform_to_add = platforms_list[platformKey];
-        Array.isArray(platform_to_add)
-          ? matrix.push(...platform_to_add)
-          : matrix.push(platform_to_add);
-      }
-    });
-  }
-  const platforms_matrix = JSON.stringify({ settings: matrix });
-  config.push(['platforms-matrix', platforms_matrix]);
-
-  // Define 'should-ci'
-  const should_ci =
-    config.find((item) => item[0] === 'skip-ci')[1] === 'true'
-      ? 'false'
-      : 'true';
-  config.push(['should-ci', should_ci]);
-
-  // Define 'should-desktop-cd'
-  const should_desktop_cd =
-    (config.find((item) => item[0] === 'always-cd')[1] === 'true' ||
-      context.eventName === 'push') &&
-    ['all', 'desktop'].includes(config.find((item) => item[0] === 'builds')[1])
-      ? 'true'
-      : 'false';
-  config.push(['should-desktop-cd', should_desktop_cd]);
-
-  // Define 'universal-version' and 'is-prerelease'
-  const version = config.find((item) => item[0] === 'version')[1];
-  let is_prerelease = 'false';
-  let universal_version = version;
-  const semver_regex = /^(\d+)\.(\d+)\.(\d+)(?:-(alpha|beta|rc)\.(\d+))?$/;
-  const match = version.match(semver_regex);
-  if (match) {
-    const [, major, minor, patch, prerelease_type, prerelease_version] = match;
-    if (prerelease_type) {
-      is_prerelease = 'true';
-      const prerelease_types = { alpha: 0, beta: 1, rc: 2 };
-      const prerelease_type_code =
-        prerelease_types[prerelease_type.toLowerCase()];
-      const encoded_patch =
-        patch * 2048 +
-        prerelease_type_code * 32 +
-        parseInt(prerelease_version, 10);
-      universal_version = `${major}.${minor}.${encoded_patch}`;
-    }
-  }
-  config.push(['universal-version', universal_version]);
-  config.push(['is-prerelease', is_prerelease]);
-}
-
-/**
- * @param {string} text
- * @param {Array<Array<string>>} config
- */
-function overrideConfigFromText(text, config) {
-  config.forEach((item) => {
-    const regex = new RegExp(`\\[${item[0]}=([^\\]]+)\\]`, 'i');
-    if (text.match(regex)) {
-      const match = text.match(regex);
-      item[1] = match[1];
-    }
-  });
-}
-
-/**
- * @param {import('@actions/core')} core
- * @param {[string, string][]} outputs
+ * - Dump the outputs to the workflow's summary
+ * - Set the outputs as outputs of this Github action
+ * @param {GithubCore} core
+ * @param {Outputs} outputs
  */
 function computeOutputs(core, outputs) {
   core.summary.addTable([
@@ -194,9 +99,19 @@ function computeOutputs(core, outputs) {
       { data: 'Name', header: true },
       { data: 'Value', header: true },
     ],
-    ...outputs.map((output) => {
-      core.setOutput(output[0], output[1]);
-      return [output[0], output[1]];
+    ...Object.entries(outputs).map(([k, v]) => {
+      core.setOutput(k, v);
+      return [k, `${v}`];
     }),
   ]);
+}
+
+/**
+ * Cast (unwrap) a non-null value to a non-null type.
+ * It is equivalent to the ! operator in Typescript (eg. `a!.b!.c`)
+ * @template T
+ * @param {T} value
+ */
+function castNonNull(value) {
+  return /** @type {NonNullable<T>} */ (value);
 }
