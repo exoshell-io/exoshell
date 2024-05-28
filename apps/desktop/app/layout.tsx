9b80ad28665c04425965c695930a45881006f84b
Charles Tison <ctison@pm.me>
2024-05-28 04:29:33 +0200

    chore(desktop): use css layers

--- apps/desktop/app/layout.tsx
+++ apps/desktop/app/layout.tsx
@@ -1,12 +1,10 @@
-import '@mantine/core/styles.css';
-
-import '@mantine/code-highlight/styles.css';
-import '@mantine/notifications/styles.css';
-import '@mantine/spotlight/styles.css';
+import '@mantine/code-highlight/styles.layer.css';
+import '@mantine/core/styles.layer.css';
+import '@mantine/notifications/styles.layer.css';
+import '@mantine/spotlight/styles.layer.css';
 import 'allotment/dist/style.css';
-import 'mantine-contextmenu/styles.css';
+import 'mantine-contextmenu/styles.layer.css';
 import 'reactflow/dist/style.css';
-
 import './layout.css';
 
 import { JotaiProvider, ReactQueryProvider } from '@/_providers';
