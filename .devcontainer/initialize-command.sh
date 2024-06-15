e1fcc223f088a46ae10ed02c9ce4d7ff133fa526
Abraham Guimbao <abrahamguimbao@gmail.com>
2024-06-15 15:00:56 +0200

    fix: fix variable quoting, remove redudant env sorting, remove recommended vscode extensions
    Signed-off-by: Abraham Guimbao <abrahamguimbao@gmail.com>

--- .devcontainer/initialize-command.sh
+++ .devcontainer/initialize-command.sh
@@ -3,14 +3,13 @@
 # Set error handling and debug
 set -eux
 
-# Combine .env and .env.local, filter out comments and empty lines, sort by variable name,
+# Combine all .env files, filters out comments and empty lines,
 # remove duplicates, and append environment variables with host information.
-(cat .env .env.local | \
- grep -v '^#' | \
- grep -v '^$' | \
- sort -t '=' -k 1,1 | \
- awk -F '=' '!seen[$1]++' && \
- echo "DEVCONTAINER_HOST_USERNAME=$(id -un)" && \
- echo "DEVCONTAINER_HOST_UID=$(id -u)" && \
- echo "DEVCONTAINER_HOST_GROUPNAME=$(id -gn)" && \
- echo "DEVCONTAINER_HOST_GID=$(id -g)") > .devcontainer/.env
+(cat .env* |
+  grep -v '^#' |
+  grep -v '^$' |
+  awk -F '=' '!seen[$1]++' &&
+  echo "DEVCONTAINER_HOST_USERNAME=$(id -un)" &&
+  echo "DEVCONTAINER_HOST_UID=$(id -u)" &&
+  echo "DEVCONTAINER_HOST_GROUPNAME=$(id -gn)" &&
+  echo "DEVCONTAINER_HOST_GID=$(id -g)") >.devcontainer/.env
