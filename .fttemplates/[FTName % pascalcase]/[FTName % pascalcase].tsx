169740a3521d2f81da255f7b753cc99044861e63
Charles Tison <ctison@pm.me>
2024-11-22 00:07:45 +0100

    chore: restore vscode folder template

---
+++ .fttemplates/[FTName % pascalcase]/[FTName % pascalcase].tsx
@@ -0,0 +1,11 @@
+import classes from './[FTName % pascalcase].module.css';
+
+export interface [FTName % pascalcase]Props {}
+
+export const [FTName % pascalcase]: React.FC<Readonly<[FTName % pascalcase]Props>> = () => {
+  return (
+    <>
+      <></>
+    </>
+  );
+}
